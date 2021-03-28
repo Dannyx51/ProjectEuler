27d5091d96a0ad9554b9e51cb085276ffbfd5861
Dhaanish K <freinds528@gmail.com>
2021-03-27 20:14:23 -0400

    update #3

--- Python Solutions/prje3.py
+++ Python Solutions/prje3.py
@@ -1,3 +1,7 @@
+from time import time
+
+st = time()
+
 a = 600851475143
 i = 2
 while i ** 2 < a:
@@ -6,3 +10,5 @@ while i ** 2 < a:
     i = i + 1
 
 print(a)
+print(f"Time taken : {time() - st}")
+#Avg Time Taken: 9.9E-4 seconds
