d2abb3bef698e0737bdfd11ebd1cb843c687e2af
Dhaanish K <freinds528@gmail.com>
2021-03-27 20:48:11 -0400

    #13 gets pythonified

--- Python Solutions/prje13.py
+++ Python Solutions/prje13.py
@@ -1,10 +1,14 @@
-txt = open("C:\\Users\\dhaan\\Documents\\PythonPrjs\\ProjectEuler\\prje13_num.txt","r")
-raw = txt.readlines()
-txt.close()
-#print(raw)
+from functools import reduce
+from time import time
 
-total = 0
-for i in raw:
-    total += int(i) 
+st = time()
+
+raw = open("Python Solutions\\prje13_num.txt","r").readlines()
+
+total = reduce(lambda x,y: int(x) + int(y), [x.replace('\n','') for x in raw])
 
 print(str(total)[0:10])
+print(f"Time taken : {time() - st}")
+#Avg Time Taken: 0 seconds??
+
+#idk either, it keeps saying 0 - 9.9E-4 so like im gonna run with the funnier number
