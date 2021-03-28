fd5d3bb66bb2a1d3938b5fa500c480f83e22e4c7
Dhaanish K <freinds528@gmail.com>
2021-03-27 20:10:58 -0400

    update #1

--- Python Solutions/prje1.py
+++ Python Solutions/prje1.py
@@ -1,14 +1,15 @@
+from time import time
 
-l = []
+st = time()
+
+total = 0 
 for i in range(1,1000):
     if i % 3 == 0:
-        if l.count(i) == 0:
-            l.append(i)
+        total += i
+        continue
     if i % 5 == 0:
-        if l.count(i) == 0:
-            l.append(i)
+        total += i
 
-total = 0
-for i in l:
-    total += i
 print(total)
+print(f"Time taken : {time() - st}")
+#Avg Time Taken: 9.6E-4 seconds
