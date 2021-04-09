7230be84ff755af59284508428c6c9baeb5c65aa
Dhaanish K <freinds528@gmail.com>
2021-04-08 23:59:57 -0400

    cleaned up 48

--- Python Solutions/prje48.py
+++ Python Solutions/prje48.py
@@ -1,6 +1,11 @@
-import math
+from time import time
+
+st = time()
+
 n = 1000
 m = 10**10
 total = sum(pow(i, i, m) for i in range(1, n + 1))
-print(total % (m))
 
+print(total % (m))
+print(f"Time taken : {time() - st}")
+#Avg Time Taken: 2E-3 seconds
