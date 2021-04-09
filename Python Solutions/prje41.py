519546d922c241f727028acedb92e2fb8089447e
Dhaanish K <freinds528@gmail.com>
2021-04-08 23:47:00 -0400

    cleaning up 41

--- Python Solutions/prje41.py
+++ Python Solutions/prje41.py
@@ -1,43 +1,29 @@
 from itertools import permutations
-raw = list(permutations("1234567"))
+from functools import reduce
+from time import time
+from primeHelper import Prime
+
+st = time()
+
+l = [int(reduce(lambda x,y: x + y, p)) for p in list(permutations("1234567"))]
 
 n = 7654321
 
-prime = [True for i in range(n+1)] 
-p = 2
-while (p * p <= n): 
-        
-    # If prime[p] is not changed, then it is a prime 
-    if (prime[p] == True): 
-            
-        # Update all multiples of p 
-        for i in range(p * p, n+1, p): 
-            prime[i] = False
-    p += 1
+prime = Prime.genPrime(n)
 
-lp = []
-for i in range(7123456, len(prime)):
-    if prime[i] == True:
-        lp.append(i)
-
-l = []
-for i in raw:
-    s = ""
-    for j in i:
-        s += j
-    l.append(int(s))
+lp = [i for i in range(7123456, len(prime)) if prime[i]]
 
 lpan = []  
 for i in l:
     gaming = False
     for ii in lp:
         if i % ii == 0:
-              gaming = True
-              break
+                gaming = True
+                break
     if gaming:
         lpan.append(i)   
 
-lpan.sort()
-print(lpan[len(lpan) - 1])
 
-#idfk it works
+print(max(lpan))
+print(f"Time taken : {time() - st}")
+#Avg Time Taken: 15.3 seconds
