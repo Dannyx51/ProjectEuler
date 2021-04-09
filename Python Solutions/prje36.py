69da57acfa8ba36324acea585e66414c57c64e7a
Dhaanish K <freinds528@gmail.com>
2021-04-08 22:48:48 -0400

    cleaned up 36

--- Python Solutions/prje36.py
+++ Python Solutions/prje36.py
@@ -1,23 +1,20 @@
+from time import time
 
-#function that checks for palindrom
 def pal(var):
     svar = str(var)
-    fh = svar[0:len(svar)//2]
-    sh = svar[len(svar)//2 if len(svar) % 2 == 0 else ((len(svar)//2)+1):]
-    if fh == sh[::-1]:
-        return True
-    else: return False
+    return svar == svar[::-1]
 
 #converts to bin
 def dectobin(n):
     return bin(n).replace("0b","")
 
-l =[]
+st = time()
+
+l = []
 for i in range(1,1000001):
     if pal(i) and pal(dectobin(i)):
         l.append(i)
 
-total = 0
-for i in l:
-    total += i
-print(total)
+print(sum(l))
+print(f"Time taken : {time() - st}")
+#Avg Time Taken: 0.35 seconds
