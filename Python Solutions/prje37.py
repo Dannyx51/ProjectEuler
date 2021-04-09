d069ca77bae27a8d17ef12cf70fcf159841d8e86
Dhaanish K <freinds528@gmail.com>
2021-04-08 22:52:01 -0400

    cleaned up 37

--- Python Solutions/prje37.py
+++ Python Solutions/prje37.py
@@ -1,17 +1,10 @@
+from primeHelper import Prime
+from time import time
+
+st = time()
 n = 1000000 #im hoping that this has the set im looking for in it
 
-prime = [True for i in range(n+1)] 
-p = 2
-while (p * p <= n): 
-        
-    # If prime[p] is not changed, then it is a prime 
-    if (prime[p] == True): 
-            
-        # Update all multiples of p 
-        for i in range(p * p, n+1, p): 
-            prime[i] = False
-    p += 1
-prime[1] = False
+prime = Prime.genPrime(n) 
 
 l = []
 for i in range(10,len(prime)):
@@ -27,9 +20,6 @@ for i in range(10,len(prime)):
   if not gaming: continue
   l.append(i)
 
-total = 0
-for i in l:
-  total += i
-print(total)
-
-#ayy my n value worked but barely
+print(sum(l))
+print(f"Time taken : {time() - st}")
+#Avg Time Taken: ~1 second
