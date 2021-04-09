b8679f9883a59e64d2895b8ccf15d59d66c34d70
Dhaanish K <freinds528@gmail.com>
2021-04-09 19:11:30 -0400

    testing diff strategies

--- Python Solutions/prje69.py
+++ Python Solutions/prje69.py
@@ -3,13 +3,33 @@ from time import time
 
 st = time()
 
-n = 10
-primes = Prime.genPrime(n, noBool=True)
+n = 50000
 
-l = {}
-for i in primes:
-    x = i
-    while x < n:
-        pass
+def v1():
+    primes = Prime.genPrime(n, noBool=True)
 
-print(l)
+    l = {}
+    for i in primes:
+        x = i
+        inc = 2
+        while x <= n:
+            if x not in l:
+                l[x] = Prime.relativePrime(x,tot = True)
+
+            x = i * inc
+            inc += 1    
+    return l
+
+def v2():
+    l = {}  
+    for i in range(2,n+1):
+        l[i] = Prime.relativePrime(i, tot = True)
+    return l
+
+st = time()
+v1()
+print(f"time taken for v1 : {time() - st}")
+
+st = time()
+v2()
+print(f"time taken for v2 : {time() - st}")
