f5866605d124660e1347d9634a5d4614e5ac6183
Dhaanish K <freinds528@gmail.com>
2021-04-15 20:34:07 -0400

    finished 46

--- Python Solutions/prje46.py
+++ Python Solutions/prje46.py
@@ -1,43 +1,24 @@
 from time import time
 from primeHelper import Prime
 
-
-arr = Prime.genPrime(100,noBool = True)
-
-def close(n:int) -> int:
-
-    if n <= arr[0]: return arr[0]
-    if n >= arr[len(arr) - 1]: return arr[len(arr) - 1]
-
-    i = 0; j = len(arr); mid = 0
-    while i < j:
-        mid = (i + j) // 2
-
-        if arr[mid] == n: return arr[mid]
-        else:
-            if n < arr[mid]:
-                if mid > 0 and n > arr[mid - 1]:
-                    v1 = arr[mid -1]; v2 = arr[mid]
-                    if n - v1 > v2 - n: return v2
-                    return v1
-                
-                j = mid
-            else:
-                if mid < len(arr) - 1 and n < arr[mid + 1]:
-                    v1 = arr[mid]; v2 = arr[mid + 1]
-                    if n - v1 > v2 - n: return v2
-                    return v1
-
-                i = mid + 1
-    return arr[mid]
 st = time()
 
+i = 3
+while True:
+    found = True
+    isP = Prime.isPrime(i)
+    if not isP:
+        for x in range(1,i):
+            n = i - 2 * (x ** 2)
+            
+            if Prime.isPrime(n):
+                found = False
+                break
+        
+    if found and not isP:
+        print(f"<< {i} >>")
+        break
+    i += 2
 
-# l = {}
-# for i in range(35,1000,2):
-#     pass
-
-
-print(close(10))
 print(f"Time taken : {time() - st}")
-#Avg Time Taken:  seconds
+#Avg Time Taken: 2E-2 seconds
