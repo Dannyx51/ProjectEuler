a04ef25fb4cc508a0c0fa8f48b18fe98bc9b4a0d
Dannyx51 <freinds528@gmail.com>
2021-04-19 07:56:13 -0400

    63 et fini

--- Python Solutions/prje63.py
+++ Python Solutions/prje63.py
@@ -1,11 +1,22 @@
+from math import ceil
 from time import time
+
+# Establishing bounds
+# --> 10^(n-1) <= x^n < 10^n
+# --> n - 1 <= log(x)n
+# --> (n - 1)/n <= log(x)n
+# --> 10^((n-1)/n) <= x
+# --> --> lim as n approaches infinity == 1
+# --> --> lim of 10^((n-1)/n) == 10
 
 st = time()
 
-l = set()
-for i in range(1,30):
-    for n in range(2,30):
-        if len(str(i**n)) == n:
-            l.add(i**n)
-            print(i,n)
+lower = 0; cur = 0; n = 1;
+while lower < 10:
+    lower = ceil(10**((n-1)/n))
+    cur += 10 - lower
+    n += 1
 
+print(cur)
+print(f"Time taken : {time() - st}")
+#Avg Time Taken: 8E-5 seconds
