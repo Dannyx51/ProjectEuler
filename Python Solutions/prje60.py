bcf2edd6dada6f550bdf0110b6b2d5c302b5a637
Dhaanish K <freinds528@gmail.com>
2021-04-19 21:25:43 -0400

    holy nested for loops

--- Python Solutions/prje60.py
+++ Python Solutions/prje60.py
@@ -1,3 +1,53 @@
 from primeHelper import Prime
+from time import time
 
-lp = Prime.genPrime(10000,noBool=True)
+isPrime = Prime.isPrime
+
+limit = 10000
+
+lp = Prime.genPrime(limit)
+
+def check(a:int,b:int) -> bool:
+    v1 = int(str(a) + str(b))
+    v2 = int(str(b) + str(a))
+    
+    pv1, pv2 = False, False
+
+    if v1 < limit: pv1 = v1 in lp
+    else: pv1 = isPrime(v1)
+
+    if v2 < limit: pv2 = v2 in lp
+    else: pv2 = isPrime(v2)
+
+    return pv1 and pv2
+
+st = time()
+
+exitloops = False
+for a in lp:
+    if exitloops: break
+    for b in lp:
+        if exitloops: break
+        if b < a: continue
+        
+        if check(a,b):
+            for c in lp:
+                if exitloops: break
+                if c < b: continue
+
+                if check(a,c) and check(b,c):
+                    for d in lp:
+                        if exitloops: break
+                        if d < c: continue
+
+                        if check(a,d) and check(b,d) and check(c,d):
+                            for e in lp:
+                                if exitloops: break
+                                if e < d: continue
+
+                                if check(a,e) and check(b,e) and check(c,e) and check(d,e):
+                                    print(a+b+c+d+e)
+                                    exitloops = True
+
+print(f"Time taken : {time() - st}")
+#Avg Time Taken: 13.2~ seconds
