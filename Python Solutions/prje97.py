d6d433a910db8711ce1a42f0a5a008f69745e38d
Dannyx51 <freinds528@gmail.com>
2021-04-21 09:17:46 -0400

    97 et fini

---
+++ Python Solutions/prje97.py
@@ -0,0 +1,18 @@
+from time import time
+from math import ceil, log
+
+st = time()
+
+number = pow(2,7830457)
+number *= 28433
+number += 1
+
+sizeN = ceil(log(number, 10))
+part = (sizeN // 235720) + (sizeN % 235720)
+number %= 10 ** part
+
+number = str(number)
+
+print(number[len(number) - 10:])
+print("Time taken :", time() - st)
+#Avg Time Taken : 0.176 seconds
