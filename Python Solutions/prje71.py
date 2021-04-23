5f61ac553dcfed8b0061c7c64041c51417554179
Dannyx51 <freinds528@gmail.com>
2021-04-23 12:15:43 -0400

    71 et fini

---
+++ Python Solutions/prje71.py
@@ -0,0 +1,17 @@
+from time import time
+
+st = time()
+
+search = 1000000
+for d in range(2,search):
+    if not d % 3: continue
+    n = round(d / 3)
+    n += 2 * d
+
+    if n > search: break
+    md, mn = n, d
+
+print(f"Target:      3 / 7      -> {3/7}")
+print(f"Found : {mn} / {md} -> {mn/md}")
+print(f"Time taken : {time() - st}")
+#Avg Time Taken: 0.5~ seconds
