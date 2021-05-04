44a2f1b3a94fea78dc5392a82f4d387420a21236
Dhaanish K <freinds528@gmail.com>
2021-05-04 18:43:38 -0400

    i dont like typing these

--- Python Solutions/prje4.py
+++ Python Solutions/prje4.py
@@ -1,27 +1,26 @@
 from time import time
 
 def isP(n):
-    s = str(n)
-    if len(s) % 2 != 0:
-        a = s[0:len(s)//2]
-        b = s[len(s)//2+1:]
-    else:
-        a = s[0:len(s)//2]
-        b = s[len(s)//2:]
-        #if n == 906609: print(a , b)
-    return(a == b[::-1])
+    return str(n) == str(n)[::-1]
     
 st = time()
 
-l = []
+ans = 0
 for i in range(100,1000):
     for j in range(100,1000):
         n = i * j
-        #if n == 906609: print(isP(n))
         if isP(n):
-            l.append(n)
-l.sort()
+            ans = n if n > ans else ans
 
-print(l[len(l)-1])
+print(ans)
 print(f"Time taken : {time() - st}")
-#Avg Time Taken: ~0.8 second
+#Avg Time Taken: ~0.44 second
+
+#Problem:
+    # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+    # Find the largest palindrome made from the product of two 3-digit numbers.
+
+#Solution:
+    # Multiply every combination of a 3 digit number * 3 digit number
+    # If the result (n) is palindromic, check to see if it's bigger than anything found previously
+    # return the largest (n) found.
