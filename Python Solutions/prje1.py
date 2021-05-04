d014e392c5e55c337fe4aa24745c6fd05b40ca1f
Dhaanish K <freinds528@gmail.com>
2021-05-04 18:11:11 -0400

    #1 get's new formatting

--- Python Solutions/prje1.py
+++ Python Solutions/prje1.py
@@ -4,12 +4,16 @@ st = time()
 
 total = 0 
 for i in range(1,1000):
-    if i % 3 == 0:
-        total += i
-        continue
-    if i % 5 == 0:
-        total += i
+    total += i if not (i % 3) or not (i % 5) else 0
 
 print(total)
 print(f"Time taken : {time() - st}")
 #Avg Time Taken: 9.6E-4 seconds
+
+
+# Problem: 
+    # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    # Find the sum of all the multiples of 3 or 5 below 1000.
+
+# Solution:
+    # loops from 1 - 999 and adds the number to total if it is divisible by 3 or 5.
