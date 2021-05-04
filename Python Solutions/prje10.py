1d4b0fa4f0fe31973acf1edee0f1f36a483e288a
Dhaanish K <freinds528@gmail.com>
2021-05-04 19:36:38 -0400

    W

--- Python Solutions/prje10.py
+++ Python Solutions/prje10.py
@@ -1,26 +1,18 @@
 from time import time
-from functools import reduce
+from primeHelper import Prime
 
 st = time()
-#sigh what is it with this site and generating primes
-#i remember my original version, it was genuinely terrible
-#i will use better practices this time
 
-n = 2000000
-prime = [True for i in range(n+1)] 
-p = 2
-while (p * p <= n): 
-        
-    # If prime[p] is not changed, then it is a prime 
-    if (prime[p] == True): 
-            
-        # Update all multiples of p 
-        for i in range(p * p, n+1, p): 
-            prime[i] = False
-    p += 1
-
-ans = reduce(lambda x,y: x + y,[x for x in range(len(prime)) if prime[x]])
+ans = sum(Prime.genPrime(2000000))
 
 print(ans)
 print(f"Time taken : {time() - st}")
-#Avg Time Taken: ~1 second
+#Avg Time Taken: 0.43~ second
+
+# Problem:
+    # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+    # Find the sum of all the primes below two million.
+
+# Solution:
+    # See primeHelper to see how the list is generated.
+    # sum the generated list.
