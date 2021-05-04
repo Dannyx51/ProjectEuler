7ac977524eeca40636cf1a6244482d8ddc454fc1
Dhaanish K <freinds528@gmail.com>
2021-05-04 19:02:44 -0400

    abra cadabra

--- Python Solutions/prje6.py
+++ Python Solutions/prje6.py
@@ -2,11 +2,25 @@ from time import time
 
 st = time()
 
-ps = pt = 0
+s = t = 0
 for i in range(1,101):
-    ps += i
-    pt += i ** 2
+    s += i
+    t += i ** 2
 
-print((ps ** 2) - pt)
+print((s ** 2) - t)
 print(f"Time taken : {time() - st}")
 #Avg Time Taken: 9.9E-4 seconds
+
+# Problem:
+    # The sum of the squares of the first ten natural numbers is: 
+        # 1**2 + 2**2 + ... + 10**2 = 385 
+    # The square of the sum of the first ten natural numbers is: 
+        # (1 + 2 + ... + 10)**2 = 55**2 = 3025
+    # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is:
+        # 3025 - 385 = 2640
+    # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+# Solution:
+    # 's' stores the sum of the natural numbers that are squared at the end
+    # 't' stores the sum of the square of the natural numbers
+    # (s**2) - t is the difference.
