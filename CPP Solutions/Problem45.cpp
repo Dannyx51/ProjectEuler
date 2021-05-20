02a1a4504e57c33daf4db63695254796ed590df1
Ethan Rudy <16.erudy@gmail.com>
2021-05-20 10:28:05 -0400

    Added timing

--- CPP Solutions/Problem45.cpp
+++ CPP Solutions/Problem45.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <cmath>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 bool check(long n);
 
 int main() {
+	auto start = high_resolution_clock::now();
 	int i = 143;
 	long n = 0;
 
@@ -13,7 +16,10 @@ int main() {
 		i++;
 		n = i * (2 * i - 1);
 		if (check(n)){
+			auto end = high_resolution_clock::now();
+			auro dura = duration_cast<milliseconds>(end - start);
 			cout << n << endl;
+			cout << "Solution found in " << dura.count() << " milliseconds" << endl;
 			break;
 		}
 	}
